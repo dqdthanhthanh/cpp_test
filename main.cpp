0b7a2295ef9cc0d366f06903f8b7e460d2b49ff3
Dung Dinh <57259604+dqdthanhthanh@users.noreply.github.com>
2023-10-15 07:18:02 +0700

    c++ basics syntax

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
+/*
+\n xuong dong
+<< endl xuong dong
+
+\t Tạo một tab ngang
+\\ Chèn ký tự dấu gạch chéo ngược (\)
+\" Chèn ký tự trích dẫn kép
+*/
+
 int main() {
-  cout << "Hello World!" << endl;
+  cout << "\tHello World!\n";
+  cout << "I am learning C++";
   return 0;
 } 
